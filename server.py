dfdf56a563d41c7aaf2d49ee9b6d1feb843d1348
Angus Grieve-Smith <angus@cs.NYU.EDU>
2016-06-26 15:45:37 -0400

    Added segment colors

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import sys, os, json, datetime
 from flask import Flask, url_for, render_template, Markup, request
 
 fr = {}
+colormap = {}
 
 app = Flask(__name__)
 
@@ -12,9 +13,8 @@ def at():
 
     fo = {
         'width': '800',
-        'ff': { 'audio_url': 'patricia-marx-interviews-john-simon.mp3' },
         'fr': fr,
-        'seg': [4, 2, 5, 3, 8]
+        'colormap': colormap
     }
     return render_template('at.html', f=fo)
 
@@ -31,6 +31,9 @@ if __name__ == "__main__":
     infile = ar[1]
     with open(infile) as jfile:
         fr = json.load(jfile)
+
+    with open('data/colormap.json') as jfile:
+        colormap = json.load(jfile)
 
     app.run(debug=True, port=19722, host='0.0.0.0')
 #    app.run( port=19722, host='0.0.0.0')
