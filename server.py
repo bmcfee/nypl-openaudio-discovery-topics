9b22515737f43ca809239895b23337de8cfc795d
Angus Grieve-Smith <angus@cs.NYU.EDU>
2016-06-26 13:06:06 -0400

    Flask server with template and peaks initialization

---
+++ server.py
@@ -0,0 +1,35 @@
+#!/home/angus/bin/python3.4
+
+import sys, os, json, datetime
+from flask import Flask, url_for, render_template, Markup, request
+
+fr = {}
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def at():
+
+    fo = {
+        'ff': { 'audio_url': 'patricia-marx-interviews-john-simon.mp3' },
+        'fr': fr,
+        'seg': [4, 2, 5, 3, 8]
+    }
+    return render_template('at.html', f=fo)
+
+
+if __name__ == "__main__":
+
+    ar = sys.argv
+
+    dateformat = "%B %d, %Y"
+
+    if (len(ar) < 2):
+        exit('Usage: at inputfile')
+
+    infile = ar[1]
+    with open(infile) as jfile:
+        fr = json.load(jfile)
+
+    app.run(debug=True, port=19722, host='0.0.0.0')
+#    app.run( port=19722, host='0.0.0.0')
